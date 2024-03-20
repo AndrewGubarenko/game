876f3644b9524d2c3379f2df658bd0d79efca276
AndrewGubarenko <AndrewGubarenko@github.com>
2024-03-20 22:02:44 +0200

    First configuration and screen setting for the game

---
+++ src/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """Class for game settings"""
+
+    def __init__(self):
+        # screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
