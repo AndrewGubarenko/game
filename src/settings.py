d6af8d017766cfb1ff79ae4e859a5e723519c73a
AndrewGubarenko <AndrewGubarenko@github.com>
2024-03-25 18:22:17 +0200

    Fighter now can move

--- src/settings.py
+++ src/settings.py
@@ -6,7 +6,10 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+        self.icon = "static/images/logo.bmp"
+        self.game_name = "Alien Invasion"
 
         # fighter settings
-        self.fighter_width = 72
-        self.fighter_height = 72
+        self.fighter_width = 55
+        self.fighter_height = 79
+        self.fighter_speed = 1.1
