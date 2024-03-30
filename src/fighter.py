7526336bb9db4fcd33d747a381a6654af3127e80
AndrewGubarenko <AndrewGubarenko@github.com>
2024-03-30 15:14:39 +0200

    Control panel is ready

--- src/fighter.py
+++ src/fighter.py
@@ -1,10 +1,12 @@
 import pygame
+from pygame.sprite import Sprite
 
 
-class Fighter:
+class Fighter(Sprite):
     """Class for fighter logic"""
 
     def __init__(self, ai_game):
+        super().__init__()
         self.settings = ai_game.settings
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
--- src/settings.py
+++ src/settings.py
@@ -13,11 +13,9 @@ class Settings:
         self.fighter_image_ref = "static/images/fighter.bmp"
         self.fighter_width = 55
         self.fighter_height = 79
-        self.fighter_speed = 1.1
         self.fighters_limit = 3
 
         # bullet settings
-        self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
@@ -31,6 +29,22 @@ class Settings:
         self.aliens_in_row_quantity = self.available_space_x // (2 * self.alien_width)
         self.available_height_y = self.screen_height - (3 * self.alien_height) - self.fighter_height
         self.aliens_row_quantity = self.available_height_y // (3 * self.alien_height)
+        self.speedup_scale = 1.1
+        self.score_scale = 1.2
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        self.fighter_speed = 1.1
+        self.bullet_speed = 1.0
         self.alien_speed = 0.5
         self.fleet_drop_speed = 3.0
+        self.alien_points = 10
+
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        self.fighter_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
